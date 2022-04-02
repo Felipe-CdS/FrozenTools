6c058e1f874dbf89e6340b9c649e749d769e5365
Felipe Coutinho <felipe32santos@hotmail.com>
2022-04-02 16:19:51 -0300

    GetCollectionByTokenAddress route bug fix

--- src/Services/GetCollectionByTokenAdressService.ts
+++ src/Services/GetCollectionByTokenAdressService.ts
@@ -6,14 +6,7 @@ class GetCollectionByTokenAdressService {
 
     async execute(token_address: string){
         const transactionRepository = getCustomRepository(TransactionRepositories);
-        const transactions = await transactionRepository.find(
-            {
-                where: { 
-                    token_address,
-                    block_timestamp: LessThan("2021-05-01T00:15:48.000Z")
-                }
-            }            
-            );
+        const transactions = await transactionRepository.find({ where: { token_address } });
         return transactions;
     }
 }

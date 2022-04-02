032aa44058a1a6776b36b9cb6e591f82af1e4002
Felipe Coutinho <felipe32santos@hotmail.com>
2022-04-02 15:39:20 -0300

    configuration for heroku deploy 0.3

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,8 @@
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
+    // "ssl": process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    "ssl": { rejectUnauthorized: false },
     "entities": [process.env.SRC_FOLDER + "/Entities/*.ts"],
     "migrations": [process.env.SRC_FOLDER + "/database/migrations/*.ts"],    
     "cli": {
